0b9b0f1f19a1297e615cca645f7d12017ff13f9a
jsm1306 <sindhu.j1729@gmail.com>
2025-01-20 13:24:54 +0530

    Frontend Initial

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); 
+app.get("/",(req,res)=>{
+    res.send("Server is ready");
+})
+app.listen(PORT,()=>{
+    console.log("Server started at http://localhost:"+PORT);
+})
