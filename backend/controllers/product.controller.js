5037eaea0da00761d80d02251126113f1ee9037c
jsm1306 <sindhu.j1729@gmail.com>
2025-02-03 09:40:14 +0530

    Backend Setup

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,46 @@
+import Product from '../models/product.model.js';
+import mongoose from 'mongoose';
+export const getProducts = async(req,res)=> {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({data:products, success:true});
+    }catch(err){
+        return res.status(500).json({message:err.message, success:false});
+    }
+}
+export const createProduct = async(req,res)=>{
+    const product = req.body;
+    if(!product.name || !product.image|| !product.price || !product.category || !product.ratings || !product.reviews || !product.specifications){
+        return res.status(400).json({message:"Please fill all the fields", success:false});
+    }
+    const newproduct = new Product(product);
+    try{
+        await newproduct.save();
+        res.status(201).json({data:newproduct, success:true});
+    }catch(err){
+        return res.status(500).json({message:err.message, success:false});
+
+    }}
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ message: "Product deleted successfully", success: true });
+    } catch (err) {
+        return res.status(500).json({ message: err.message, success: false });
+    }
+}
+export const updateProduct=async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({message:"No product with that id", success:false});
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        await Product.findByIdAndUpdate(id, updatedProduct, { new: true });
+        res.status(200).json({ data: updatedProduct, success: true });
+    } catch (err) {
+        res.status(500).json({ message: err.message, success: false });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
+import productRoutes from "./routes/product.routes.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); 
-app.get("/",(req,res)=>{
-    res.send("Server is ready");
-})
+app.use("/api/products", productRoutes);
 app.listen(PORT,()=>{
     connectDB();
     console.log("Server started at http://localhost:"+PORT);
